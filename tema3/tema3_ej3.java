f0d35eea9c84f7d9df71180181b59909478de16e
cristinator <75039823+cristinator@users.noreply.github.com>
2020-11-25 19:42:05 +0100

    Add files via upload

---
+++ tema3/tema3_ej3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class tema3_ej3 {
+  
+    public static void main(String[] args) {
+        double a = 8;
+        double b = 3;
+        double div = (a / b) % (10);
+        System.out.println("el primer resultado es" + a / b);
+        System.out.println("el primer resultado es" + div);
+    }
+}
